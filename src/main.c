084954837313c7e72e731f618e50e77d52ad10df
João Dias <joao.dias@edge.ufal.br>
2026-08-21 14:59:55 -0300

    chore: estruturação do repositório

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*******************************************************************
+ * @file main.c
+ *
+ * @brief Main file of zbook abstraction.
+ * @author João Matheus Nascimento Dias (joao.dias@edge.ufal.br)
+ * @version 0.1
+ * @date 21/08/2026
+ *
+ * @copyright Copyright (c) 2026
+ *
+ *******************************************************************/
+
+#include <stdio.h>
+
+int main(void) 
+{
+  printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+  return 0;
+}
